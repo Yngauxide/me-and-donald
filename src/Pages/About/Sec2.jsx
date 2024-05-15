d4bfc594dc3945737c7c58bc9403d0d75d27077a
Donald Yusuf <donalddyusuf@gmail.com>
2024-05-15 08:26:09 -0700

    Pages

---
+++ src/Pages/About/Sec2.jsx
@@ -0,0 +1,20 @@
+const Sec2 =() => {
+    return (
+        <>
+        <div className='container flex lg:flex-row-reverse flex-col justify-center w-screen gap-[4.4rem] items-center mt-[3rem]'>
+    <div class=''>
+        <p className='text-[#E3FFCC] lg:text-left text-center lg:leading-[3rem] leading-10'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis <br />
+            ipsum quae tenetur totam exercitationem quam architecto nulla <br />
+            sapiente assumenda cum. Laborum culpa quidem tempore eius veniam <br />
+            libero adipisci molestias quisconsequatur modi? Est, a magnam? Non,<br />
+            ratione praesentium voluptatibus laudantium ipsa, mollitia,<br />
+            illo dolores iure nemo in aspernatur aliquam dignissimos.
+        </p>
+    </div>
+        <img class='lg:w-[40%] w-[90%]' src="src\assets\images\about-2.png" />
+</div>
+        </>
+    )
+};
+export default Sec2
